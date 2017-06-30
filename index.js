aff79d2b297e5ca5391f93e300671c8b8d40bf91
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2017-06-30 16:15:24 +0200

    Reference table only from cited sources
    Code style

--- index.js
+++ index.js
@@ -1,95 +1,114 @@
-var bibtexParse = require('bibtex-parser-js');
-var _ = require('lodash');
-var fs = require('fs');
+'use strict'
+
+const bibtexParse = require('bibtex-parser-js')
+const fs = require('fs')
+
+let currBib = []
 
 module.exports = {
-    book: {
-        assets: './assets',
-        css: ["style.css"]
-    },
+  book: {
+    assets: './assets',
+    css: ["style.css"]
+  },
+  filters: {
+    cite: function(key) {
 
-    filters: {
-        cite: function(key) {
-            var citation = _.find(this.config.get('bib'), {'citationKey': key.toUpperCase()});
+      let ret = "[Citation not found]"
 
-            var path = this.config.get('pluginsConfig')['bibtex-indexed-cite'].path;            
+      const upperKey = key.toUpperCase()
+      const citation = find(this.config.get('bib'), {'citationKey': upperKey})
 
-            if (citation !== undefined) {
-                
-                var index = _.indexOf(this.config.get('bib'), citation) + 1;    
-                                
-                return '<a href="' + path + 'References.html#cite-' + index + '">[' + index + ']</a>';
+      const path = this.config.get('pluginsConfig')['bibtex-indexed-cite'].path            
 
-            } else {
-                return "[Citation not found]";
-            }
+      if (citation !== undefined) {
+
+        var index = currBib.indexOf(upperKey)
+        if (index === -1) {
+          currBib.push(upperKey)
+          index = currBib.length
+        }else{
+          index++
         }
-    },
 
-    hooks: {
-        init: function() {
-            var bib = fs.readFileSync('literature.bib', 'utf8');
-            this.config.set('bib', bibtexParse.toJSON(bib));
-            this.config.set('bibCount', 0);                     
-        }
-    },
-
-    blocks: {
-        references: {
-            process: function(blk) {
-                var bib = this.config.get('bib');
-                           
-                var result = '<table class="references">';
-
-                bib.forEach(function(item) {                
-
-                    var index = _.indexOf(bib, item) + 1;                
-
-                    result += '<tr><td><span class="citation-number" id="cite-' + index + '">' + index + '</span></td><td>';
-
-                    if (item.entryTags.AUTHOR) {
-                        result += formatAuthors(item.entryTags.AUTHOR) + ', ';
-                    }
-                    if (item.entryTags.TITLE) {
-                        if (item.entryTags.URL) {
-                            result += '<a href="' + item.entryTags.URL + '">' + item.entryTags.TITLE + '</a>, ';
-                        } else {
-                            result += item.entryTags.TITLE + ', ';
-                        }
-                    }
-                    if (item.entryTags.BOOKTITLE) {
-                        if (item.entryTags.BOOKURL) {
-                            result += '<a href="' + item.entryTags.BOOKURL + '">' + item.entryTags.BOOKTITLE + '</a>, ';
-                        } else {
-                            result += '<i>' + item.entryTags.BOOKTITLE + '</i>, ';
-                        }
-                    }
-                    if (item.entryTags.PUBLISHER) {
-                        result += '<i>' + item.entryTags.PUBLISHER + '</i>, ';
-                    }
-                    if (item.entryTags.YEAR) {
-                        result += item.entryTags.YEAR + '.';
-                    }
-
-                    result += '</td></tr>';
-                });
-
-                result += '</table>';
-
-                return result;
-            }
-        }
+        // This path should probably not be here
+        ret = `<a href="${path}References.html#cite-${index}">[${index}]</a>`
+      }
+      return ret
     }
-};
+  },
+  hooks: {
+    init: function() {
+      const bib = fs.readFileSync('literature.bib', 'utf8')
+      this.config.set('bib', bibtexParse.toJSON(bib))
+      currBib = []
+    }
+  },
+  blocks: {
+    references: {
+      process: function(blk) {
 
+        if (!currBib.length) {
+          return
+        }
 
+        const bib = this.config.get('bib')
+        let result = '<table class="references">'
+
+        currBib.forEach(function(cite, idx) {
+
+          const item = find(bib, { citationKey: cite })
+          const index = idx + 1
+
+          result += `<tr><td><span class="citation-number" id="cite-${index}">${index}</span></td><td>`
+
+          if (item.entryTags.AUTHOR) {
+            result += formatAuthors(item.entryTags.AUTHOR) + ', '
+          }
+          if (item.entryTags.TITLE) {
+            if (item.entryTags.URL) {
+              result += `<a href="${item.entryTags.URL}">${item.entryTags.TITLE}</a>, `
+            } else {
+              result += item.entryTags.TITLE + ', '
+            }
+          }
+          if (item.entryTags.BOOKTITLE) {
+            if (item.entryTags.BOOKURL) {
+              result += `<a href="${item.entryTags.BOOKURL}">${item.entryTags.BOOKTITLE}</a>, `
+            } else {
+              result += `<i>${item.entryTags.BOOKTITLE}</i>, `
+            }
+          }
+          if (item.entryTags.PUBLISHER) {
+            result += `<i>${item.entryTags.PUBLISHER}</i>, `
+          }
+          if (item.entryTags.YEAR) {
+            result += item.entryTags.YEAR + '.'
+          }
+
+          result += '</td></tr>'
+        })
+
+        result += '</table>'
+
+        return result
+      }
+    }
+  }
+}
+
+function find (arr, search) {
+  let i = 0;
+  const key = Object.keys(search)[0]
+  for(; i < arr.length && arr[i][key] !== search[key]; i++);
+  return i === arr.length ? undefined : arr[i]
+}
 
 function formatAuthors (authorsString) {
-    var authors = authorsString.split('and');
+  const authors = authorsString.split('and')
 
-    if (authors.length > 3) {
-        return authors[0] + ' <i>et al.</i>';
-    } else {
-        return authorsString;
-    }
+  if (authors.length > 3) {
+    return authors[0] + ' <i>et al.</i>'
+  } else {
+    return authorsString
+  }
 }
